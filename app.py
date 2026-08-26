96d59a9febb125348064020d2ed91fec83976027
Oleksandr Kravchuk <Kravtmk@gmail.com>
2026-08-26 10:40:16 +0000

    Initial MyApp v1

---
+++ app.py
@@ -0,0 +1,12 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"<h1>MyApp Docker v1</h1>")
+
+server = HTTPServer(("0.0.0.0", 8000), Handler)
+print("MyApp Docker v1 running on port 8000")
+server.serve_forever()
